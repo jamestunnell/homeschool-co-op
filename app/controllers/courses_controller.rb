ca202faed890a74fb6ad62872db64839dcbd32c9
James Tunnell <jamestunnell@gmail.com>
2015-02-05 06:40:35 -0800

    Fix more form bugs

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -54,6 +54,6 @@ class CoursesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def course_params
-      params.require(:course).permit(:title, :description, :subject_id)
+      params.require(:course).permit(:title, :description, :subject_id, :workshop)
     end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -53,6 +53,6 @@ class SessionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def session_params
-      params.require(:session).permit(:term, :kind, :start_date, :end_date)
+      params.require(:session).permit(:term, :kind, :start_date, :end_date, :workshop)
     end
 end
