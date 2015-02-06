bb8e4b8eb15dda516d0f7afc8a0b40395d515f4e
James Tunnell <jamestunnell@gmail.com>
2015-02-05 17:58:24 -0800

    Add first/last name to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, :confirmable
+         
+  validates_presence_of :first
+  validates_presence_of :last
+  validates_length_of :first, :minimum => 2, :maximum => 36
+  validates_length_of :last, :minimum => 2, :maximum => 36
+  
+  def full_name
+    "#{first} #{last}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206010640) do
+ActiveRecord::Schema.define(version: 20150206013615) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street"
@@ -159,6 +159,8 @@ ActiveRecord::Schema.define(version: 20150206010640) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first"
+    t.string   "last"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
